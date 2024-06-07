5442598941a0d9827872e542e0da5a404a9726d4
b09780978 <b09780978@gmail.com>
2024-06-08 06:01:04 +0800

    Update lazy.lua

--- lua/kai/lazy.lua
+++ lua/kai/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "kai.plugins" },  } , {
+require("lazy").setup({ { import = "kai.plugins" }, { import = "kai.plugins.lsp" }, } , {
   checker = {
     enabled = true,
     notify = false,
